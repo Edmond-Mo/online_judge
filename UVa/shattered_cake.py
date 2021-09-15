f3b03f886187fdd78c0083a859f8ea2d0fde4ac6
Edmond-Mo <86791087+Edmond-Mo@users.noreply.github.com>
2021-09-14 21:43:07 -0700

    Delete shattered_cake.py

--- UVa/shattered_cake.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-getc = stdin.readline
-W = int(getc())
-N = int(getc())
-area = 0
-for _ in range(N):
-    w, l = (int(i) for i in getc().split())
-    area += w * l
-print(area//W)
